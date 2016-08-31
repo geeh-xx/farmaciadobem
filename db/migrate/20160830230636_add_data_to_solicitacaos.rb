6e4c558a4e8166290c25a5c299bada11f40b5cc9
Geeh <rangel.soares.souza@gmail.com>
2016-08-31 16:35:02 -0300

    area de admin

--- db/migrate/20160830230636_add_data_to_solicitacaos.rb
+++
@@ -1,5 +0,0 @@
-class AddDataToSolicitacaos < ActiveRecord::Migration
-  def change
-    add_column :solicitacaos, :data_criacao, :date
-  end
-end
--- db/migrate/20160831001257_remove_date_to_solicitacaos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateToSolicitacaos < ActiveRecord::Migration
-  def change
-    remove_column :solicitacaos, :data_criacao, :date
-  end
-end
